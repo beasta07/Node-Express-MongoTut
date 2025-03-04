2a8a4c017c7a75f29e6d4d7d0321045a67156276
Pranish Bista <pranish@sysqube.com.np>
2025-03-04 18:01:47 +0545

    Node First Introductions , Creating Basic Api & Routes

---
+++ index.js
@@ -0,0 +1,57 @@
+const fs = require("fs");
+const http = require("http");
+const url = require("url");
+////////FILES
+// const textIn=fs.readFileSync('./txt/input.txt','utf-8')
+// console.log(textIn);
+
+// const textOut = `This is what we know about the avacado : ${textIn} \n Created on ${Date.now()}`
+// fs.writeFileSync('./txt/output.txt',textOut)
+
+// console.log('File has been written');
+
+//Non-blocking
+// fs.readFile('./txt/start.txt','utf-8', (err,data1)=> {
+// fs.readFile(`./txt/${data1}.txt`,'utf-8', (err,data2)=> {
+//     console.log(data2)
+//     fs.readFile(`./txt/append.txt`,'utf-8', (err,data3)=> {
+//         console.log(data3)
+//         fs.writeFile('./txt/final.txt',`${data2}\n${data3}`, 'utf-8', err => {
+// console.log('Your file has been written')
+//         })
+//     } )
+// } )
+// } )
+// console.log('Will READ File')
+
+/////////
+
+/////////////////////SERVER
+
+const data = fs.readFileSync(
+  `${__dirname}/dev-data/data.json`,
+  "utf-8",
+  (err, data) => {
+    const dataObj = JSON.parse(data);
+  }
+);
+
+const server = http.createServer((req, res) => {
+  const pathName = req.url;
+  if (pathName === "/overview") {
+    res.end("This is the overvoew");
+  } else if (pathName === "/product") {
+    res.end("This is the PRODUCT");
+  } else if (pathName === "/api") {
+    res.end(data);
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "my-own-header": "hello-world",
+    });
+    res.end("<h1>Page not found</h1>");
+  }
+});
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to requests on port 8000");
+});
